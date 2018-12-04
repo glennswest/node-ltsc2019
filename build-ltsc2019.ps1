54ca3b22bf5acf91ba97c2b15d6dbf49b9e24689
gwest <gwest.redhat.com>
2018-12-03 22:52:34 -0800

    Fix name

--- build-ltsc2019.ps1
+++ build-ltsc2019.ps1
@@ -1,1 +1,2 @@
-docker build -t glennswest/node:10.14.0-windowsservercore-ltsc2019 .
+docker build -t glennswest/node-windows:10.14.0-windowsservercore-ltsc2019 .
+docker push  glennswest/node-windows:10.14.0-windowsservercore-ltsc2019
